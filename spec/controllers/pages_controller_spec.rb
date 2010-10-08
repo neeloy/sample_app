bf76c5ee633b54e36ef0529f0a89bcdba94a8851
Neeloy Roy <neeloy@berkeley.edu>
2010-10-08 03:41:07 -0700

    Starting ch5

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,15 +4,20 @@ describe PagesController do
 
   render_views
   
+  before(:each) do
+    @base_title="Ruby on Rails Tutorial Sample App"
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+
     it "should have the right title" do
-    get 'home'
-    response.should have_selector("title",
-                   :content=>"Ruby on Rails Tutorial Sample App | Home")
+      get 'home'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Home")
     end
   end
 
@@ -21,23 +26,38 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                      :content=>"Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => @base_title + " | Contact")
     end
   end
-  
+
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
+
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                      :content=>"Ruby on Rails Tutorial Sample App | About")
+                                    :content => @base_title + " | About")
     end
-  end 
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                      :content=> @base_title + " | Help")
+    end
+  end
+  
   
 end
